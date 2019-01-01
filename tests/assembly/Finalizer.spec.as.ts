ce8cd99f3fb30e5deb66d435287820c8298c78c3
kazuya kawaguchi <kawakazu80@gmail.com>
2019-01-02 01:42:22 +0900

    :shirt: refactor: finalizer spec

--- tests/assembly/Finalizer.spec.as.ts
+++ tests/assembly/Finalizer.spec.as.ts
@@ -4,11 +4,11 @@ import { Finalizer } from '../../assembly/Finalizer'
 
 export class FinalizerTests {
   static shouldBeCall (): bool {
-    let handler = (data: Array<u8>): void => {
+    let handler = (data: u8[]): void => {
       data[0] = 0x02
     }
-    let buf: Array<u8> = [0x01]
-    let finalizer = new Finalizer<Array<u8>>(handler, buf)
+    let buf: u8[] = [0x01]
+    let finalizer = new Finalizer<u8[]>(handler, buf)
     finalizer.call()
     return buf[0] == 0x02
   }
