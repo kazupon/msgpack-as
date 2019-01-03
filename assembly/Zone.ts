1e3391666f7025f7440c75d0c5428530e6aa2c52
kazuya kawaguchi <kawakazu80@gmail.com>
2019-01-04 01:18:30 +0900

    :construction: WIP: add zone implemenetation stubs

--- assembly/Zone.ts
+++ assembly/Zone.ts
@@ -1,13 +1,39 @@
 import { ChunkList } from './ChunkList'
 import { FinalizerArray } from './FinalizerArray'
 
+const ZONE_CHUNK_SIZE: i32 = 8192
+const ZONE_ALIGN: i32 = sizeof<usize>()
+
 export class Zone {
   private chunkSize: i32
-  private chunkList: ChunkList
-  private finalizerArray: FinalizerArray
+  private chunkList: ChunkList<Array<u8>>
+  private finalizerArray: FinalizerArray<Array<u8>>
 
-  constructor () {
+  constructor (chunkSize: i32 = ZONE_CHUNK_SIZE) {
+    this.chunkSize = chunkSize
+    this.chunkList = new ChunkList<Array<u8>>(chunkSize)
+    this.finalizerArray = new FinalizerArray<Array<u8>>()
+  }
+
+  allocateAlign (size: i32, align: i32 = ZONE_ALIGN): usize {
     // TODO:
+    return changetype<usize>(this)
+  }
+
+  allocateNoAlign (size: i32): usize {
+    // TODO:
+    return changetype<usize>(this)
+  }
+
+  pushFinalizer (handler: (data: Array<u8>) => void, data: Array<u8>): void {
+    // TODO:
+  }
+
+  clear (): void {
+    // TODO:
+  }
+
+  swap (zone: Zone): void {
   }
 
   dispose (): void {
